b7cc6df3ec80bbd981806c8f20712de72b64dd9c
Harsh Sharma <harsharma7190@gmail.com>
2026-04-22 22:12:05 +0530

    fix minor error in deployment config section

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -3,6 +3,6 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const ENV = {
-    NODE_ENV=process.env.NODE_ENV,
-    PORT=process.env.PORT
+    NODE_ENV: process.env.NODE_ENV,
+    PORT: process.env.PORT
 }
