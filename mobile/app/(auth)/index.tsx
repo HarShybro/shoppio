04c33731ebafb0411cc1b6ecdaf1541caee55f4f
Harsh Sharma <harsharma7190@gmail.com>
2026-04-26 22:17:05 +0530

    mobile setup

---
+++ mobile/app/(auth)/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const AuthtScreen = () => {
+  return (
+    <View>
+      <Text>AuthtScreen</Text>
+    </View>
+  );
+};
+
+export default AuthtScreen;
